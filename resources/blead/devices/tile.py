69208e1e89fdc0b9f5b3592995d1bd0088368a0e
Hugo Questroy <hquestroy@evertygo.com>
2019-01-20 19:19:53 +0100

    battery level

--- resources/blead/devices/tile.py
+++ resources/blead/devices/tile.py
@@ -16,5 +16,25 @@ class Tile():
 		action={}
 		action['present'] = 1
 		return action
+    
+    def read(self,mac):
+		result={}
+		try:
+			conn = Connector(mac)
+			conn.connect(type='random')
+			if not conn.isconnected:
+				conn.connect(type='random')
+				if not conn.isconnected:
+					return
+			batteryDatas = bytearray(conn.readCharacteristic('0x17'),type='random')
+			result['battery'] = batteryDatas[0]
+			result['id'] = mac
+			logging.debug(str(result))
+			return result
+		except Exception,e:
+			logging.error(str(e))
+			conn.disconnect()
+		conn.disconnect()
+		return result
 
 globals.COMPATIBILITY.append(Tile)
